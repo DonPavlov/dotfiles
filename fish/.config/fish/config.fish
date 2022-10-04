fc604364cf660422348575750e64a493f1bab58b
DonPavlov <1541649+DonPavlov@users.noreply.github.com>
2022-10-04 21:37:27 +0200

    add old dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,20 +3,10 @@ set -x PATH $PATH /sbin/
 set -x PATH $PATH /home/paul/Skripte
 set -x LANG "de_DE.UTF-8"
 
-#add rust tools
-set -x PATH $PATH /home/paul/.cargo/bin
-
 #correct behavior for ssh agent
 eval 'ssh-agent -s'
 
 ssh-add ~/.ssh/id_rsa
-ssh-add ~/.ssh/git_ed25519
-ssh-add ~/.ssh/hetzneruser_priv
-ssh-add ~/.ssh/id_ed25519
-ssh-add ~/.ssh/pi_buntu
-ssh-add ~/.ssh/github_eos
-
-clear
 
 alias g "googler -n 7 -l DE"
 
@@ -27,5 +17,3 @@ function sudo
         command sudo $argv
     end
 end
-
-thefuck --alias | source
